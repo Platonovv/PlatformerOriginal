9b15b2561a06beba6852f6dbac66582cf8e377ad
Platonovv <Diablo-98@inbox.ru>
2022-03-27 19:10:42 +0700

    commit StartLearning 2 weeks
    Начало обучения 2 недели, работа с гитхабом

---
+++ platformer/Assets/PixelCrew/Hero.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+   [SerializeField] private float _speed;
+   [SerializeField] private float _jumpSpeed;
+   [SerializeField] private LayerCheck _groundCheck;
+   
+
+   private Rigidbody2D _rigidbody;
+   private Vector2 _direction;
+
+
+   private void Awake()
+   {
+      _rigidbody = GetComponent<Rigidbody2D>();
+   }
+
+   public void SetDirection(Vector2 direction)
+   {
+      _direction = direction;
+      
+   }
+
+   private void FixedUpdate()
+   {
+      _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+
+      var isJumping = _direction.y > 0;
+      if (isJumping )
+      {
+         if (ItsGrounded())
+         {
+            _rigidbody.AddForce(Vector2.up * _jumpSpeed,ForceMode2D.Impulse);
+         }
+      } else if (_rigidbody.velocity.y>0)
+      {
+         _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+      }
+   }
+
+   private bool ItsGrounded()
+   {
+      return _groundCheck.IsTouchingLayer;
+
+
+   }
+
+   public void SaySomething()
+   {
+      Debug.Log("Something!");
+   }
+}
---
+++ platformer/Assets/PixelCrew/HeroInputReader.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+
+namespace PixelCrew
+{
+
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        var  direction = context.ReadValue<Vector2>();
+        _hero.SetDirection(direction);
+        
+        
+    }
+    
+    public void OnSaySomething(InputAction.CallbackContext context)
+    {
+        if (context.canceled)
+        {
+            _hero.SaySomething();
+        }
+
+    }
+
+   
+}
+}
