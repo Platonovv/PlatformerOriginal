a19173305dd6f056e896336138e67bfdcc01f52a
Platonovv <Diablo-98@inbox.ru>
2022-05-04 17:32:56 +0700

    Курсовая №3
    Призыв акул у краба

--- platformer/Assets/PixelCrew/Creatures/Mobs/CloneMobsAI.cs
+++ platformer/Assets/PixelCrew/Creatures/Mobs/CloneMobsAI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PixelCrew.Components.Audio;
 using PixelCrew.Components.ColliderBased;
 using UnityEngine;
 using Object = System.Object;
@@ -11,14 +12,22 @@ namespace PixelCrew.Creatures.Mobs
         [SerializeField] public GameObject obj;
         [SerializeField] private ColliderCheck _vision;
         public float _NARUTA = 0;
+        private PlaySoundsComponent _sounds;
+
+
+        private void Awake()
+        {
+            _sounds = GetComponent<PlaySoundsComponent>();
+        }
 
         public void Update()
         {
-            if (_vision.IsTouchingLayer && _NARUTA <= 1)
+            if (_vision.IsTouchingLayer && _NARUTA >= 1)
             {
-                    var naruto = Instantiate(obj, transform.position, Quaternion.identity);
-                    _NARUTA++;
-                    naruto.SetActive(true);
+                _sounds.Play("Jutsu");    
+                var naruto = Instantiate(obj, transform.position, Quaternion.identity);
+                _NARUTA--;
+                naruto.SetActive(true);
             }
             
         }
