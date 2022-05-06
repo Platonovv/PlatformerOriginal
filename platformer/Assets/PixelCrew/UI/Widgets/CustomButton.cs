74b10ac4d9260e573fa10e5273853dcab6c063c7
Platonovv <Diablo-98@inbox.ru>
2022-05-06 15:01:32 +0700

    Введение в UI
    Домашки

---
+++ platformer/Assets/PixelCrew/UI/Widgets/CustomButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+
+namespace PixelCrew.UI.Widgets
+{
+    public class CustomButton : Button
+    {
+        [SerializeField] private GameObject _normal;
+        [SerializeField] private GameObject _pressed;
+
+
+        protected override void DoStateTransition(SelectionState state, bool instant)
+        {
+            base.DoStateTransition(state, instant);
+            
+            _normal.SetActive(state != SelectionState.Pressed);
+            _pressed.SetActive(state == SelectionState.Pressed);
+
+        }
+    }
+}
